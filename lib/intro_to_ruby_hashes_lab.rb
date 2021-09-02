026b40a773d3f496a7512e3ad0d744450a41874f
Sheridan <sheridanmaustad@gmail.com>
2021-09-01 21:49:47 -0600

    commit tree f0bc50a8aab2b0cae6d661d9774e7ef9f9da2d66

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,22 @@
 def new_hash
-  # return an empty hash
+ {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+#   person = {
+#   name: "Sam",
+#   age: 31
+# }
+{ :favorite_color => "purple" }
 end
 
 def pioneer
+  { name: "Grace Hopper" }
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
+  { id: number }
   # return a hash with a key :id assigned to the provided number
-end
+end 
